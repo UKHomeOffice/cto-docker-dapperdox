18715e953634eee515ab4f7d777d3b9862775e18
philbarton <philbart@mac.com>
2019-08-01 12:20:06 +0100

    Add ref data example

--- bin/runDocker.sh
+++ bin/runDocker.sh
@@ -3,7 +3,7 @@
 docker run --rm \
     --name cto-dapperdox \
     -p3123:3123 \
-    -e SPEC_FILENAME=uber/swagger.json \
+    -e SPEC_FILENAME="refdata/swagger.json,uber/swagger.json" \
     -e WAIT=1 \
     -v $PWD/example_specs:/go/bin/specs \
     -d cto-dapperdox:latest
